675e3408f4b854cdee62324780395e0dce7ceec4
Anthony Picciano <anthony.picciano@gmail.com>
2014-08-11 15:35:58 -0600

    Initial Commit

---
+++ Bears/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bears
+//
+//  Created by Anthony Picciano on 8/11/14.
+//  Copyright (c) 2014 Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BearsTests/BearsTests.m
@@ -0,0 +1,34 @@
+//
+//  BearsTests.m
+//  BearsTests
+//
+//  Created by Anthony Picciano on 8/11/14.
+//  Copyright (c) 2014 Picciano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BearsTests : XCTestCase
+
+@end
+
+@implementation BearsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
