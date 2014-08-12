67b0b4dfdbec3406578bc58d3f9beb5462b2cf1f
Anthony Picciano <anthony.picciano@gmail.com>
2014-08-12 08:36:22 -0600

    set styles for log in and sign up

--- Bears/Views/MainViewController.m
+++ Bears/Views/MainViewController.m
@@ -48,15 +48,28 @@
 
 - (void)logInOrCreateAccount
 {
+    // log in
     PFLogInViewController *viewController = [[PFLogInViewController alloc] init];
     UIImageView *logoView = [[UIImageView alloc] init];
     logoView.image = [UIImage imageNamed:@"logo"];
     [logoView sizeToFit];
     viewController.logInView.logo = logoView;
+    viewController.logInView.backgroundColor = [UIColor colorWithRed:1.0/256.0 green:29.0/256.0 blue:68.0/256.0 alpha:1.0];
+    viewController.logInView.usernameField.backgroundColor = [UIColor whiteColor];
+    viewController.logInView.passwordField.backgroundColor = [UIColor whiteColor];
     
+    // sign up
     viewController.signUpController.fields = PFSignUpFieldsUsernameAndPassword | PFSignUpFieldsSignUpButton | PFSignUpFieldsDismissButton;
     viewController.delegate = self;
     viewController.signUpController.delegate = self;
+    UIImageView *logoView2 = [[UIImageView alloc] init];
+    logoView2.image = [UIImage imageNamed:@"logo"];
+    [logoView2 sizeToFit];
+    viewController.signUpController.signUpView.logo = logoView2;
+    viewController.signUpController.signUpView.backgroundColor = [UIColor colorWithRed:1.0/256.0 green:29.0/256.0 blue:68.0/256.0 alpha:1.0];
+    viewController.signUpController.signUpView.usernameField.backgroundColor = [UIColor whiteColor];
+    viewController.signUpController.signUpView.passwordField.backgroundColor = [UIColor whiteColor];
+    
     [self presentViewController:viewController animated:YES completion:nil];
 }
 
